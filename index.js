c31fb49f4200f79a78e5e38f79cdbcd5db060739
piyanshujain <piyanshujain02@gmail.com>
2021-04-04 19:32:53 +0530

    added torrent parser and modified some functions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const bencode = require('bencode');
 const tracker = require('./tracker');
+const torrentParser = require('./torrent-parser');
 
 const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
 
